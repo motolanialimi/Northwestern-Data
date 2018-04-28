4e46452b089caf9344767ca449af9b580db424ca
Motols A <mgbenro@gmail.com>
2018-04-28 14:18:13 -0500

    Adding more files :-)

---
+++ Untitled-1.py
@@ -0,0 +1,13 @@
+'''Basic Variables
+
+# Create two different variables that will take the input of your first name and your neighbors first name.
+
+MyName= input("what is your name?")
+NeighborsName= input("what is your neighbors name?")
+
+'''Create two more inputs that will ask how many months each of you have been coding.
+
+time1= int(input("how many months have you been coding?"))
+
+'''Finally, display a result with both your names and the total amount of months coding.
+print("I am " + MyName and "my neighbors name is" + NeighborsName)
